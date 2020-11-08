c1ea29e949156d60dfd602e5525f29beb31fb921
ProgrammingPredator <69820851+ProgrammingPredator@users.noreply.github.com>
2020-11-08 15:14:47 +0100

    Add files via upload

---
+++ build/precache-manifest.6aba1b4a21bd709f49e1d31757e81ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12dbf750e47bef505e82c32381d9e77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9472ffab52f58cbe388",
+    "url": "/static/css/main.e0bb075d.chunk.css"
+  },
+  {
+    "revision": "6b6b610f91b037360584",
+    "url": "/static/js/2.1b025511.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1b025511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9472ffab52f58cbe388",
+    "url": "/static/js/main.cc198439.chunk.js"
+  },
+  {
+    "revision": "42236b3c5a0e9054fb96",
+    "url": "/static/js/runtime-main.0c82e416.js"
+  }
+]);
